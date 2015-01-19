b3cef48746c82127669342bde24b6f17a06583d5
vijaykakadiya <vijay.kakadiya10@gmail.com>
2015-01-19 01:08:12 -0500

    reformatting

--- src/bitpostions/Main.java
+++ src/bitpostions/Main.java
@@ -3,44 +3,42 @@ package bitpostions;
 import java.io.*;
 import java.util.*;
 
+/**
+* Bitposition challenge problem.
+*
+* @author Vijay Kakadiya
+*/
 public class Main {
 
-	public static void main(String[] args) {
-	
-		String path = args[0];
-		String line = null;
-		try{
-		FileReader fileReader = new FileReader(path);
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
+  public static void main(String[] args) {
+    String path = args[0];
+	String line = null;
+	try {
+	  FileReader fileReader = new FileReader(path);
+	  BufferedReader bufferedReader = new BufferedReader(fileReader);
 		
-		while((line = bufferedReader.readLine()) != null){
-			String[] inputArgs = line.split(",");
-				if(inputArgs.length!=3)
-				{
-					System.out.println("File not correct. Input in wrong format\n'");
-					break;
-				}
-				Integer n = Integer.parseInt(inputArgs[0]);
-				Integer p1 = Integer.parseInt(inputArgs[1]);
-				Integer p2 = Integer.parseInt(inputArgs[2]);
-				boolean result = bitPositions(n, p1, p2);
-				System.out.println(result);
-			}
-		}//try ends here
-		
-		catch(Exception ex){
-			System.out.println("Unable to open file '" + path +ex+ "'");
+	  while ((line = bufferedReader.readLine()) != null) {
+	    String[] inputArgs = line.split(",");
+		if (inputArgs.length!=3) {
+		  System.out.println("File not correct. Input in wrong format\n'");
+		  break;
 		}
-
+		Integer n = Integer.parseInt(inputArgs[0]);
+		Integer p1 = Integer.parseInt(inputArgs[1]);
+		Integer p2 = Integer.parseInt(inputArgs[2]);
+		boolean result = bitPositions(n, p1, p2);
+		System.out.println(result);	
+		}
+	} catch (Exception ex) {
+	    System.out.println("Unable to open file '" + path +ex+ "'");
 	}
+  }
 	
-	private static boolean bitPositions(Integer number, Integer pos1, Integer pos2) {
-		if(((number << (32 - pos1) >> 31) ^ (number << (32 - pos2) >> 31)) == 0 ){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-
+  private static boolean bitPositions (Integer number, Integer pos1, Integer pos2) {
+    if (((number << (32 - pos1) >> 31) ^ (number << (32 - pos2) >> 31)) == 0 ) {
+	  return true;
+	  } else {
+	      return false;
+	  }
+  }    
 }
